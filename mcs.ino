84452bf6454d6fe4e983860b77ed776e1bb9b9fd
Lidarsheu <lidarsheu@gmail.com>
2018-12-20 11:10:39 +0800

    Update mcs.ino

--- mcs.ino
+++ mcs.ino
@@ -5,10 +5,10 @@
 int t;
 int h;
 
-char _lwifi_ssid[] = "basstomouth";
-char _lwifi_pass[] = "peter1224";
+char _lwifi_ssid[] = "MediaTek_Labs";
+char _lwifi_pass[] = "84149961";
 
-MCSDevice mcs("DeKxX1KO", "9RO0PaUX0npSfzl8");
+MCSDevice mcs("Drsls7CQ", "kKUGahHDR5Hnp5Nn");
 MCSDisplayInteger temp("temp");
 MCSDisplayInteger humid("humid");
 
@@ -20,11 +20,11 @@ void setup()
 
   mcs.addChannel(temp);
   mcs.addChannel(humid);
-  Serial.println("Wi-Fi start");
+  Serial.println("Wi-Fi 開始連線");
   while (WiFi.begin(_lwifi_ssid, _lwifi_pass) != WL_CONNECTED) { delay(1000); }
-  Serial.println("Wi-Fi ok");
+  Serial.println("Wi-Fi 連線成功");
   while(!mcs.connected()) { mcs.connect(); }
-  Serial.println("MCS ok");
+  Serial.println("MCS 連線成功");
   Serial.begin(9600);
   __dht2.begin();
 }
@@ -32,8 +32,8 @@ void setup()
 void loop()
 {
   while (!mcs.connected()) {
-    mcs.connect();
-    if (mcs.connected()) { Serial.println("MCS restart"); }
+  	mcs.connect();
+  	if (mcs.connected()) { Serial.println("MCS 已重新連線"); }
   }
   mcs.process(100);
 
@@ -42,9 +42,9 @@ void loop()
   temp.set(t);
   humid.set(h);
 
-  Serial.print("c : ");
+  Serial.print("攝氏溫度 : ");
   Serial.println(t);
-  Serial.print("relative temp :");
+  Serial.print("相對溼度 :");
   Serial.println(h);
 
   delay(1000);
